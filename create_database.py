16a4241b44535555d924ec39d1969b6e13c4d0a9
PranavPusarla <36783092+PranavPusarla@users.noreply.github.com>
2020-01-01 20:49:12 -0800

    Rename create_database.py to database/create_database.py

--- create_database.py
+++
@@ -1,5 +0,0 @@
-import sqlite3
-
-connector = sqlite3.connect('weights.db')
-cursor = connector.cursor()
-cursor.execute('''CREATE TABLE WEIGHTS([Client_Name] text, [Weight] double, [Datetime] integer)''')
